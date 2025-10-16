b89e645e71599d280e135a44b5d1dc3026107ff3
Corentin Thomasset <corentin.thomasset74@gmail.com>
2025-10-16 21:27:28 +0200

    feat: add ping endpoint (#38)

--- astro.config.ts
+++ astro.config.ts
@@ -1,14 +1,17 @@
+import cloudflare from '@astrojs/cloudflare';
 import mdx from '@astrojs/mdx';
-import sitemap from '@astrojs/sitemap';
 
+import sitemap from '@astrojs/sitemap';
 import astroExpressiveCode from 'astro-expressive-code';
 import { defineConfig } from 'astro/config';
 import UnoCSS from 'unocss/astro';
 import { config } from './src/app.config';
+
 import createRedirectsFile from './src/plugins/redirects';
 
 export default defineConfig({
   site: 'https://papra.app',
+
   integrations: [
     UnoCSS({ injectReset: true }),
     sitemap(),
@@ -35,4 +38,7 @@ export default defineConfig({
     }),
     mdx(),
   ],
+
+  output: 'static',
+  adapter: cloudflare(),
 });
---
+++ src/pages/ping.ts
@@ -0,0 +1,16 @@
+export const prerender = false;
+
+export async function GET() {
+  return new Response(
+    JSON.stringify({
+      status: 'ok',
+      timestamp: new Date().toISOString(),
+    }),
+    {
+      status: 200,
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    },
+  );
+}
