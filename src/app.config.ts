b08e8d754cac4e0c9f696c705f331b423984b677
Corentin Thomasset <corentin.thomasset74@gmail.com>
2025-10-24 15:06:56 +0200

    feat(config): simplify launch logic

--- src/app.config.ts
+++ src/app.config.ts
@@ -1,18 +1,12 @@
-const isLaunched = true;
-
-export const requestEarlyAccessLink = 'https://tally.so/r/npz6BE';
-const getStartedLink = 'https://dashboard.papra.app';
-
 export const config = {
   site: {
     name: 'Papra',
   },
-  isLaunched,
   discordInvite: 'https://discord.gg/8UPjzsrBNF',
   sponsorLink: 'https://github.com/sponsors/CorentinTh',
-  getStartedLink: isLaunched ? getStartedLink : requestEarlyAccessLink,
+  getStartedLink: 'https://dashboard.papra.app',
   launchDiscount: {
-    enabled: isLaunched,
+    enabled: true,
     percentage: 50,
     // 31 december 2025 Paris time
     endsAt: new Date('2025-12-31T23:59:59+01:00'),
