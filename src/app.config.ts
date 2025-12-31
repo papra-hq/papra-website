677c818a3b61bd110fd5ce01bf24323339abc880
Corentin Thomasset <corentin.thomasset74@gmail.com>
2026-01-01 00:50:31 +0100

    refactor(discount): remove launch discount feature and related components (#55)

--- src/app.config.ts
+++ src/app.config.ts
@@ -5,10 +5,4 @@ export const config = {
   discordInvite: 'https://discord.gg/8UPjzsrBNF',
   sponsorLink: 'https://github.com/sponsors/CorentinTh',
   getStartedLink: 'https://dashboard.papra.app',
-  launchDiscount: {
-    enabled: true,
-    percentage: 50,
-    // 31 december 2025 Paris time
-    endsAt: new Date('2025-12-31T23:59:59+01:00'),
-  },
 } as const;
